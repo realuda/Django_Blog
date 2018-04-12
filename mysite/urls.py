64aeb3c4823f3adf320d42dcd2cd7854e2fc57cc
realuda <hsuper189@gmail.com>
2018-04-12 19:24:03 +0900

    Changed the HTML for the site2

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^&', view.post_list, name='post_list'),
+    url(r'^&', views.post_list, name='post_list'),
 ]
